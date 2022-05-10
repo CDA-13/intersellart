0e61dda822a4692f7f6a36da800055d0f2d5da12
Florian Jordany <jordany.florian@gmail.com>
2022-05-10 17:21:02 +0200

    first commit

--- server/App.js
+++ server/App.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 3001
 const cors = require('cors');
 const path = require('path');
+const db = require('database');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
