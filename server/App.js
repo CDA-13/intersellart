71d83c8878ed92929240754a8ce189e0dff0223b
StevyDevWeb <stevywebdev@gmail.com>
2022-05-11 17:24:14 +0200

    login and signup

--- server/App.js
+++ server/App.js
@@ -4,6 +4,7 @@ const port = 3001
 const cors = require('cors');
 const path = require('path');
 const db = require('database');
+const userRoute = require('./Routes/userRoute');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -12,5 +13,7 @@ app.use(express.json());
 db();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`connexion successfully`));
